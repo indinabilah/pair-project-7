c4c2d74b102536c5f1387e971b2620992a5a1577
User <SultanRayhan32>
2019-07-03 16:27:07 +0700

    crud

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const port = 3001
 const router = require('./routes/all-router')
 const routerTicket = require('./routes/Ticket')
+const routerMovie = require('./routes/movies') 
 
 app.use(express.urlencoded({extended: false}))
 app.listen(port, () => console.log(`Server port ${port} is running`))
 
-app.use("/", router.User)
-app.use('/ticket' ,routerTicket)
+app.use("/", router.User);
+app.use("/ticket" ,routerTicket);
+app.use("/movie" , routerMovie );
 app.get("/home", (req, res)=>{
     res.render("home.ejs")
 })
--- controllers/Ticket.js
+++ controllers/Ticket.js
@@ -1,7 +1,17 @@
 class Tickets {
     static RenderTicket(req,res){
-        res.render('TIcket.ejs' , { id : '' })
+        res.render('TIcket.ejs')
+    }
+
+    static AddTicket(req,res){
+        res.send('oe')
+        // console.log(req.body)
+        console.log(req.query)
     }
 }
+
+// user beli tiket kirim id user ke table transaksi
+// user memilih tiket dapat id yang dipilih 
+// <== insert into tb_transaksi id user .then = > insert into ticketTransaksi  
 
 module.exports = Tickets;
